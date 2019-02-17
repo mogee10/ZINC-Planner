d3231d05017654c0bc12ad2cc590a04751a7b734
Octowl <alsaff1987@gmail.com>
2019-02-17 15:45:13 +0300

    Review Comments

--- events/forms.py
+++ events/forms.py
@@ -2,27 +2,31 @@ from django import forms
 from django.contrib.auth.models import User
 from .models import Event
 
+
 class UserRegisterForm(forms.ModelForm):
-	class Meta:
-		model = User
-		fields = [ 'username', 'first_name', 'last_name', 'password']
-		widgets = {
-			'password': forms.PasswordInput(),
-		}
+    class Meta:
+        model = User
+        fields = ['username', 'first_name', 'last_name', 'password']
+        widgets = {
+            'password': forms.PasswordInput(),
+        }
+
 
 class UserLoginForm(forms.Form):
-	username = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Username'}))
-	password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Password'}))
-	fields = ['username', 'password']
-	widgets = {
-			'password': forms.PasswordInput(),
-		}
+    username = forms.CharField(widget=forms.TextInput(
+        attrs={'placeholder': 'Username'}))
+    password = forms.CharField(widget=forms.PasswordInput(
+        attrs={'placeholder': 'Password'}))
+    fields = ['username', 'password']
+    widgets = {
+        'password': forms.PasswordInput(),
+    }
 
 
 class EventForm(forms.ModelForm):
-	class Meta:
-		model = Event
-		exclude = ['owner',]
-		widgets = {
-			'date': forms.DateInput(attrs={"type": "date"}),
-		}
+    class Meta:
+        model = Event
+        exclude = ['owner', ]  # FIX ME
+        widgets = {
+            'date': forms.DateInput(attrs={"type": "date"}),
+        }
