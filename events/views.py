eadf9fe17676e494456016143fabc1e337353989
Shaha Al-Khamees <sakaac@Shahas-MacBook.local>
2019-02-17 11:20:58 +0300

    fixed urls

--- events/views.py
+++ events/views.py
@@ -50,40 +50,40 @@ def noaccess(request):
 	return render(request, 'noaccess.html')
 
 def user_register(request):
-    register_form = UserRegisterForm()
-    if request.method == "POST":
-        register_form = UserRegisterForm(request.POST)
-        if register_form.is_valid():
-            user = register_form.save(commit=False)
-            user.set_password(user.password)
-            user.save()
-            login(request, user)
-            return redirect('event-list')
-    context = {
-        "register_form": register_form
-    }
-    return render(request, 'user_register.html', context)
+	register_form = UserRegisterForm()
+	if request.method == "POST":
+		register_form = UserRegisterForm(request.POST)
+		if register_form.is_valid():
+			user = register_form.save(commit=False)
+			user.set_password(user.password)
+			user.save()
+			login(request, user)
+			return redirect('event-list')
+	context = {
+		"register_form": register_form
+	}
+	return render(request, 'user_register.html', context)
 
 def user_login(request):
-    login_form = UserLoginForm()
-    if request.method == "POST":
-        login_form = UserLoginForm(request.POST)
-        if login_form.is_valid():
-            username = login_form.cleaned_data['username']
-            password = login_form.cleaned_data['password']
-            authenticated_user = authenticate(username=username, password=password)
-            if authenticated_user:
-                login(request, authenticated_user)
-                return redirect('event-list')
-    context = {
-        "login_form": login_form
-    }
-    return render(request, 'user_login.html', context)
+	login_form = UserLoginForm()
+	if request.method == "POST":
+		login_form = UserLoginForm(request.POST)
+		if login_form.is_valid():
+			username = login_form.cleaned_data['username']
+			password = login_form.cleaned_data['password']
+			authenticated_user = authenticate(username=username, password=password)
+			if authenticated_user:
+				login(request, authenticated_user)
+				return redirect('event-list')
+	context = {
+		"login_form": login_form
+	}
+	return render(request, 'user_login.html', context)
 
 def user_logout(request):
-    logout(request)
+	logout(request)
 
-    return redirect('event-list')
+	return redirect('event-list')
 
 
 
